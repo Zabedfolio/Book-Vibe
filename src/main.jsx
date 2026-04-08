113e6dadece20eebb2eaf9a803bf36373d535f9d
Zabedfolio <zabedfolio@gmail.com>
2026-04-08 21:22:11 +0600

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import MainLayout from './layout/MainLayout';
-import Homepage from './pages/homepage/Homepage';
-import Books from './pages/books/Books';
+import { RouterProvider } from 'react-router';
+import { router } from './routes/Routes';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-    children: [
-      {
-        index: true,
-        element: <Homepage></Homepage>
-      },
-      {
-        path: "/books",
-        element: <Books></Books>
-      }
-    ]
-  }
-]);
+
 
 
 createRoot(document.getElementById('root')).render(
